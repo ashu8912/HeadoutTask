dc4d511abd0c664581e3f88a6b46292778c097a1
ashu8912 <ashughildiyal5@gmail.com>
2019-11-28 17:30:03 +0530

    added company logo

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Hamburger from "../UI/Hamburger/Hamburger";
+import Logo from '../Logo/Logo';
+import "./header.css"
 export default function Header() {
   return (
-    <div>
+    <div className="header">
     <Hamburger/>
-    {/*<Logo/>
+    <Logo/>
+    {/*
     <SearchBar/>
     <Menu/>*/}
     </div>
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Logo() {
+  return (
+    <>
+    <img src="http://cdn2.hubspot.net/hub/69576/file-17500656-png/images/you-tube-logo.png?t=1364738137000" height="100" width="150"/>
+    </>
+  );
+}
