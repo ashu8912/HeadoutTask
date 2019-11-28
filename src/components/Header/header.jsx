0faf054f98a42ba34fa4d96356cc5aef6158b4d6
ashu8912 <ashughildiyal5@gmail.com>
2019-11-28 17:15:32 +0530

    hamburger icon added

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hamburger from "../UI/Hamburger/Hamburger";
+export default function Header() {
+  return (
+    <div>
+    <Hamburger/>
+    {/*<Logo/>
+    <SearchBar/>
+    <Menu/>*/}
+    </div>
+  );
+}
